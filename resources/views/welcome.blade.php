4f60b84729414d33a98a1ffb84f92083620af98f
RogerCarrilloIH <carrillo.rogelioih@gmail.com>
2021-10-24 06:55:34 -0500

    Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 
         <style>
             body {
-                font-family: 'Nunito', sans-serif;
+                font-family: '', sans-serif;
             }
         </style>
     </head>
